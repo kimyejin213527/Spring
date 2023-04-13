24e751eee74835a5ab86b51f0b332d188047fe17
yejin <yejin213527@gmail.com>
2023-04-13 12:19:32 +0900

    0413 수업 예제

--- src/main/java/study/mvc/MvcApplication.java
+++ src/main/java/study/mvc/MvcApplication.java
@@ -2,6 +2,7 @@ package study.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MvcApplication {
@@ -9,5 +10,4 @@ public class MvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
 	}
-
 }
--- src/main/java/study/mvc/TestControoler.java
+++ src/main/java/study/mvc/TestControoler.java
@@ -13,6 +13,43 @@ public class TestControoler {
     @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
     public String hello() {
         return "Hello,World";
+
     }
-//    @RequestParam(value = "re", required = false, defaultValue = "true") Boolean showComments
+
+    @GetMapping(value = "/reverse", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String reverse (@RequestParam(value = "words", required = true) String words){
+        String result = "";
+
+        String[] strs = words.split(",");
+        for (int i = strs.length - 1; i >= 0; i--) {
+            result += strs[i] + ",";
+        }
+        return result;
+    }
+    @GetMapping (value="/mul", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String mul(
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ){
+        return  "" + (num1 * num2);
+    }
+    @GetMapping(value = "/calc/{op}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String calc(
+            @PathVariable("op") String op,
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ) throws Exception {
+            switch (op){
+                case "add" : return "" + (num1+num2);
+                case "sub" : return "" + (num1-num2);
+                case "mul" : return "" + (num1*num2);
+                default : throw new Exception("잘못된 연산자");
+        }
+    }
+    int count = 0;
+    @PostMapping("/count")
+    public String count(){
+        return "" + (++count);
+    }
 }
+
